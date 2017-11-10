ee28fd96fb2dfdd7ba75e0c366bedb417002473f
Declan Sharkey <dsharkey@pnimedia.com>
2017-11-09 17:06:23 -0800

    remove item method implemented

--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -7,26 +7,34 @@ class OrderSummary extends React.Component {
 
     constructor() {
         super();
-
-        //what I'm doing here is loading my products list, into the state
-        this.state={
-            products:sampleProducts
+        this.removeItem = this.removeItem.bind(this);
+        this.state = {
+            products: sampleProducts
         };
-        
+
     }
+
+    //remove an item from state
+    removeItem(key) {
+        const products = { ...this.state.products };
+        delete products[key];
+        this.setState({ products });
+        console.log("pressed");
+    }
+
     render() {
         return (
             <section className="order-summary">
-            <div class="order-summary-title">
-                <h3>Order Summary</h3>
-            <span class="change-order">Change</span>
-          </div>
+                <div className="order-summary-title">
+                    <h3>Order Summary</h3>
+                    <span className="change-order">Change</span>
+                </div>
                 <div className="my-orders-wrapper">
-                {
-                    Object
-                        .keys(this.state.products)
-                        .map(key => <Products key={key} details={this.state.products[key]}/> )
-                }
+                    {
+                        Object
+                            .keys(this.state.products)
+                            .map(key => <Products removeItem={this.removeItem} key={key} details={this.state.products[key]} />)
+                    }
                 </div>
             </section>
         )
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {formatPrice} from '../helpers';
-
 class Products extends React.Component {
+
     render() {
         const {details} = this.props;
         return (
@@ -10,6 +10,7 @@ class Products extends React.Component {
                 <h4>{details.name}</h4>
                 <span className="item-price">{formatPrice(details.price)}</span>
                 <span className="item-quantity">Quantity: {details.quantity}</span>
+                <span onClick={() => this.props.removeItem()} className="remove-item">Remove</span>
             </div>
         )
     }
